db5d012b47150faf1cf4c8a12479338ec69f7749
Lilly Ohr <lillyohr@gmail.com>
2024-03-05 12:55:33 -0500

    Change cross validation between Firebase Auth state and cookie state to check every 5 minutes

--- client/src/providers/UserProvider.jsx
+++ client/src/providers/UserProvider.jsx
@@ -3,28 +3,57 @@ import { useState, useEffect } from "react";
 import { UserContext } from "./UserContext";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../config/firebaseConfig";
+import { signOut } from "firebase/auth";
+import { toast } from "react-toastify";
+
+const logoutUser = async () => {
+  try {
+    await signOut(auth);
+    await axios.post("/api/sessionLogOut", {}, { withCredentials: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const shouldRevalidateSession = () => {
+  const sessionCache = localStorage.getItem("sessionCache");
+  if (!sessionCache) return true;
+
+  const { timestamp } = JSON.parse(sessionCache);
+  const currentTime = Date.now();
+  const fiveMinutes = 5 * 60 * 1000;
+  return currentTime - timestamp > fiveMinutes;
+};
+
+const setSessionCache = (isAuthenticated) => {
+  const sessionCache = {
+    isAuthenticated,
+    timestamp: Date.now(),
+  };
+  localStorage.setItem("sessionCache", JSON.stringify(sessionCache));
+};
 
 const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    let unsubscribe = null;
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user || null);
+    });
 
     const checkState = async () => {
       try {
-        const response = await axios.get("/api/auth/validateSession");
-        const { isAuthenticated } = response.data;
-
-        if (isAuthenticated) {
-          unsubscribe = onAuthStateChanged(auth, (user) => {
-            setCurrentUser(user || null);
-          });
-        } else {
-          setCurrentUser(null);
+        if (shouldRevalidateSession()) {
+          const response = await axios.get("/api/auth/validateSession");
+          const { isAuthenticated } = response.data;
+          setSessionCache(isAuthenticated);
         }
       } catch (error) {
         console.error("Error validating session:", error);
+        localStorage.removeItem("sessionCache");
         setCurrentUser(null);
+        toast.error("Authentication error. Please login again.");
+        logoutUser();
       }
     };
 
